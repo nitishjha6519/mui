b54d08c89a817f3265a8acfca9bcd750de2a071c
Nitish jha <nitishjha6519@gmail.com>
2023-02-01 13:56:54 +0530

    Rating added

--- src/components/TourCard.js
+++ src/components/TourCard.js
@@ -3,6 +3,9 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { AccessTime } from "@mui/icons-material";
+import Rating from "@mui/material/Rating";
+
 import "./TourCard.css";
 
 const TourCard = (props) => {
@@ -18,6 +21,48 @@ const TourCard = (props) => {
           <Typography variant="subtitle2" component="h2">
             Immerse into the Falls
           </Typography>
+
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <AccessTime sx={{ width: "12.5px" }} />
+            <Typography variant="body2" component="h2" marginLeft={1}>
+              5 Hours
+            </Typography>
+          </Box>
+
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+            }}
+            marginTop={2}
+          >
+            <Rating
+              name="read-only"
+              value={4.5}
+              readOnly
+              precision={0.5}
+              size="small"
+            />
+
+            <Typography variant="body2" component="h2" marginLeft={1}>
+              4.5
+            </Typography>
+
+            <Typography variant="body2" component="h2" marginLeft={1}>
+              (655 Reviews)
+            </Typography>
+          </Box>
+
+          <Box>
+            <Typography variant="h6" component="h2" marginLeft={1}>
+              From $99
+            </Typography>
+          </Box>
         </Box>
       </Paper>
     </Grid>
